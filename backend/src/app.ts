72faabf5d7e8280ae09ede722b3d75a6f3d70516
Lokesh <lokeshnegi649@gmail.com>
2024-02-06 18:39:09 +0530

    resolve cors issue

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,11 +4,13 @@ import colors from "colors";
 import morgan from "morgan";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 
 //* middlewares
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(morgan("dev")); // todo: remove this in production
